f4d6a597dbf3c255938c1db5379da73324dbdd3c
Basil Majdi <bmajdi@stevens.edu>
2017-03-04 15:30:27 -0500

    code refactored, new function added, 3 disabled

--- SSW_555/Rob_funcs.py
+++ SSW_555/Rob_funcs.py
@@ -11,24 +11,24 @@ from datetime import datetime
 def convert_2date(date1): #this function converts the givin date format to the required format
     return datetime.strptime(date1, '%d-%b-%Y')    
 
-def birth_b4_marg(dob, dom):# this function takes the dates of birth and marriage(if any) and 
+'''def birth_b4_marg(dob, dom):# this function takes the dates of birth and marriage(if any) and 
                             # returns true if the date of marriage is before date of birth 
     if dom == "NA" or dob == "NA" or dom == "ERR" or dob == "ERR": 
         return True
     elif convert_2date(dom) < convert_2date(dob):    
         return False
     else:    
-        return True
+        return True'''
 
 
-def birth_b4_dth(dob, dod): # this function takes the dates of birth and death(if any) and 
+'''def birth_b4_dth(dob, dod): # this function takes the dates of birth and death(if any) and 
                             # returns true if the date of death is before date of birth    
     if dod == "NA" or dob == "NA" or dod == "ERR" or dob == "ERR": 
         return False
     elif convert_2date(dod) < convert_2date(dob):    
         return True
     else:    
-        return False
+        return False'''
 
 
 ''' @ROBERT: COVERED IN SUDHANSH.PY VALID_DATE '''
@@ -40,23 +40,23 @@ def dates_b4_today(date_): #this function compares any givin date to today's dat
     else:    
         return True
 
-def marg_b4_dvors(doe, dom):# this function takes the dates of marriage and divorce (if any) and 
+'''def marg_b4_dvors(doe, dom):# this function takes the dates of marriage and divorce (if any) and 
                             # returns false if the date of divorce is before date of marriage
     if doe == "NA" or dom == "NA" or doe == "ERR" or dom == "ERR":
         return True   
     elif convert_2date(doe) < convert_2date(dom):    
         return False
     else:    
-        return True
+        return True'''
 
-def marg_b4_dth(dom, dod):
+'''def marg_b4_dth(dom, dod):
     # this function checks if the date of marriage happened after death. 
     if dom == "NA" or dod == "NA": 
         return True
     elif convert_2date(dod) < convert_2date(dom):    
         return False
     else:    
-        return True
+        return True'''
 
 def date_check(date_big, date_small): 
     if date_big == "NA" or date_small == "NA" or date_big == "ERR" or date_small == "ERR":
