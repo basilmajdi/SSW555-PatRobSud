2107ce24ac91894084acaa73cb92869bc743aa91
Sudhansh Aggarwal <sudhansh@sudhansh.com>
2017-03-23 18:48:48 -0400

    Changed the way we display errors

--- SSW_555/RobPratSud_Proj03.py
+++ SSW_555/RobPratSud_Proj03.py
@@ -63,13 +63,16 @@ class individual:
             # checks if date format is correct and that date is not in future 
             self.err.append("US42-DOD")
             self.dod="ERR"
+            errors.append("ERROR: INDIVIDUAL. US-42. Check DOD for "+self.pid)
         if not valid_date(dob):
             # checks if date format is correct and that date is not in future
             self.err.append("US42-DOB")
             self.dob="ERR"
+            errors.append("ERROR: INDIVIDUAL. US-42. Check DOB for "+self.pid)
         if not date_check(self.dod, self.dob):
             # checks for birth before death 
-            self.err.append('US03')   
+            self.err.append('US03')
+            errors.append("ERROR: INDIVIDUAL. US-03. DOB after DOB for "+self.pid)   
 
         # Calculate Age
         self.age=calculate_age(self.dob,self.dod)
@@ -78,6 +81,7 @@ class individual:
         if self.age > 150:
             # checks if the age is not more than 150
             self.err.append('US07')
+            errors.append("ERROR: INDIVIDUAL. US-07. Age is too much "+self.pid)
             
     def showinfo(self):
         print('{} : {}'.format("ID",self.pid))
@@ -89,7 +93,8 @@ class individual:
         print('{} : {}'.format("FAMS",self.fams))
 
     def update_table(self):
-        x.add_row([self.pid,self.name,self.sex,self.dob,self.dod,self.age,self.famc,self.fams,self.err])
+        x.add_row([self.pid,self.name,self.sex,self.dob,self.dod,self.age,self.famc,self.fams])
+        #x.add_row([self.pid,self.name,self.sex,self.dob,self.dod,self.age,self.famc,self.fams,self.err])
 
 class family:
     def __init__(self):
@@ -114,63 +119,80 @@ class family:
         if sibling_nos(self.cid)>=16:
             # checks that there are no more than 15 siblings 
             self.err.append("US15")
+            errors.append("ERROR: FAMILY. US-15. Too many childern in family "+self.fid)
         if not valid_date(dom):
             # checks if date format is correct and that date is not in future for date of marriage 
             self.err.append("US42-DOM")
             self.dom="ERR"
+            errors.append("ERROR: FAMILY. US-42. Check DOM for "+self.Fid)
         if not valid_date(doe):
             # checks if date format is correct and that date is not in future for date of divorce
             self.err.append("US42-DOE")
             self.doe="ERR"
+            errors.append("ERROR: FAMILY. US-42. Check DOE for "+self.fid)
         ## Check if couple is married before getting a divorce
         if not date_check(self.doe, self.dom):
             #checks for divorce before marriage dates 
             self.err.append('US04')
+            errors.append("ERROR: INDIVIDUAL. US-04. DIVORCED BEFORE MARRIAGE for "+self.fid)
         h=self.hid
         w=self.wid
 
         #Checking gender roles
         if gender_roles(individuals[indi.index(h)].sex,1):
             self.err.append('US21-Husband')
+            errors.append("ERROR: FAMILY. US-21. Check gender of husband; "+h+" of family "+self.fid)
         if gender_roles(individuals[indi.index(w)].sex,0):
             self.err.append('US21-Wife')
+            errors.append("ERROR: FAMILY. US-21. Check gender of wife; "+w+" of family "+self.fid)
         
         ## Check if husband is born before getting married
         if not date_check(self.dom, individuals[indi.index(h)].dob):
             # checks for marriage before birth dates for husband 
             self.err.append('US02-Husb')
+            errors.append("ERROR: FAMILY. US-02. Husband "+h+" of family "+self.fid+" married before being born")
         ## Check if wife is born before getting married
         if not date_check(self.dom, individuals[indi.index(w)].dob):
             # checks for marriage before birth dates for wife
             self.err.append('US02-Wife')
+            errors.append("ERROR: FAMILY. US-02. Wife "+w+" of family "+self.fid+" married before being born")
         if not date_check(individuals[indi.index(h)].dod, self.dom):
             # checks for marriage before death dates for husband
             self.err.append('US05-Husb')
+            errors.append("ERROR: FAMILY. US-05. Husband "+h+" of family "+self.fid+" married after dying")
         if not date_check(individuals[indi.index(w)].dod, self.dom):
             # checks for marriage before death dates for wife
             self.err.append('US05-Wife')
+            errors.append("ERROR: FAMILY. US-05. Wife "+w+" of family "+self.fid+" married after dying")
         if not date_check(individuals[indi.index(h)].dod, self.doe):
             # checks for divorce before death dates for husband
             self.err.append('US06-Husb')
+            errors.append("ERROR: FAMILY. US-06. Husband "+h+" of family "+self.fid+" divorced after dying")
         if not date_check(individuals[indi.index(w)].dod, self.doe):
             # checks for divorce before death dates for wife
             self.err.append('US06-Wife')
+            errors.append("ERROR: FAMILY. US-06. Wife "+w+" of family "+self.fid+" divorced after dying")
         #For each child in that family
         for i in self.cid: 
             if not date_check(individuals[indi.index(i)].dob, self.dom):
                 # checks for marriage of parents before birth dates  
                 self.err.append('US08')
+                errors.append("ERROR: FAMILY. US-08. child "+i+" of family "+self.fid+" born before parents marrying")
             if not date_check(individuals[indi.index(i)].dob, individuals[indi.index(h)].dod):
                 # checks for death of parents before birth dates for father 
                 self.err.append('US09-Deceased Father')
+                errors.append("ERROR: FAMILY. US-09. child "+i+" of family "+self.fid+" born significantly after the father "+h+" died")
             if not date_check(individuals[indi.index(i)].dob, individuals[indi.index(w)].dod):
                 # checks for death of parents before birth dates for mother 
                 self.err.append('US09-Deceased Mother')
+                errors.append("ERROR: FAMILY. US-09. child "+i+" of family "+self.fid+" born significantly after the mother "+w+" died")
             # check father/mother are not too old compared to the child
             if not check_age(individuals[indi.index(i)].age,individuals[indi.index(h)].age,0):
                 self.err.append("US12-Father")
+                errors.append("ERROR: FAMILY. US-12. child "+i+" of family "+self.fid+" too young for father "+h)
             if not check_age(individuals[indi.index(i)].age,individuals[indi.index(w)].age,1):
                 self.err.append("US12-Mother")
+                errors.append("ERROR: FAMILY. US-12. child "+i+" of family "+self.fid+" too young for mother "+w)
         
     def cout(self):
         print('{} : {}'.format("ID",self.fid))
@@ -182,19 +204,20 @@ class family:
         print('{} : {}'.format("No. of Siblings",self.sib_no))
 
     def update_table(self):
-        y.add_row([self.fid,self.hid,self.wid,self.dom,self.doe,self.cid,self.err])
+        y.add_row([self.fid,self.hid,self.wid,self.dom,self.doe,self.cid])
+        #y.add_row([self.fid,self.hid,self.wid,self.dom,self.doe,self.cid,self.err])
 
 
 ### VALID TAGS IN gedcom FILES ###
 tags = [ "INDI" , "FAM" , "NAME" , "SEX" , "BIRT" , "DEAT" , "FAMC" , "FAMS" , 
 "DATE" , "MARR" , "HUSB" , "WIFE" , "CHIL" , "DIV" ]
 
-filename=input("Enter the location of the file: ")
+#filename=input("Enter the location of the file: ")
 #filename = open ( "smith_tree1.ged" )
 #filename="/Users/sudhansh/Desktop/CS-555/test1.ged" #For testing purposes
 #filename="/Users/sudhansh/git/SSW_555/smith_tree1.ged" #For testing purposes
 #filename="/Users/sudhansh/Desktop/CS-555/Proj01_SudhanshAggarwal_CS555.ged"
-#filename="/Users/sudhansh/git/SSW_555/test_RPS.ged"
+filename="/Users/sudhansh/git/SSW_555/test_RPS.ged"
 
 ### CHECKING IF GEDCOM IS ENTERED, HELP TAKEN FORM AKSHAY SUNDERWANI ###
 path = os.getcwd ( )  # method to fetch working directory path.
@@ -206,6 +229,8 @@ if basefilename2[ -1 ] != '.ged':
 
 indi=[] #List of all individual IDs
 famillia=[] #List of all family IDs
+
+errors=[] #list will contain all errors present in the gedcom
 
 c_ind = c_fam = 0 # for counting the total no.s of individuals and families resp
 c_ind1 = c_fam1 = 0 # for keeping track of indi/fams
@@ -242,6 +267,7 @@ try:
                     # Checking unique IDs
                     if no_reps(indi,iden,flag):
                         individuals[c_ind1].err.append("US22")
+                        errors.append("ERROR: INDIVIDUAL. US-22. Individual ID "+iden+" not unique")
 
                     # Appending indivial's ID to indi[], making easier to find this id later
                     # We can get the loaction of the individual in individuals[] be finding it's position in the list.
@@ -253,6 +279,7 @@ try:
                     # Checking unique IDs
                     if no_reps(famillia,iden,flag):
                         families[c_fam1].err.append("US22")
+                        errors.append("ERROR: FAMILY. US-22. Family ID "+iden+" not unique")
 
                     #Appending family's ID to famillia[], making easier to find this ID later.
                     # We can get the location of the family in families[] from it's location in the list
@@ -315,6 +342,7 @@ try:
             if not sibs_no_marry(families[i].hid,families[i].wid,families[j].cid) and i!=j:
                 families[i].err.append("US18")
                 families[j].err.append("US18")
+                errors.append("ERROR: FAMILY. US-18. People "+families[i].hid+" and "+families[i].wid+" are married in family "+families[i].fid+" and are siblings in "+families[j].fid)
 
     #Checking cousins don't marry
 
@@ -328,26 +356,32 @@ try:
 
     #UPDATE PRETTY TABLE
     x = prettytable.PrettyTable() # For people
-    x.field_names = ["I.D.","NAME","SEX","BIRTH","DEATH","AGE","FAM_C","FAM_S","ERRORS"]
+    x.field_names = ["I.D.","NAME","SEX","BIRTH","DEATH","AGE","FAM_C","FAM_S"]
+    #x.field_names = ["I.D.","NAME","SEX","BIRTH","DEATH","AGE","FAM_C","FAM_S","ERRORS"]
     y = prettytable.PrettyTable() # For Families
-    y.field_names = ["I.D.","HUSBAND","WIFE","MARRIAGE","MARR_END","CHILDREN","ERRORS"]
+    y.field_names = ["I.D.","HUSBAND","WIFE","MARRIAGE","MARR_END","CHILDREN"]
+    #y.field_names = ["I.D.","HUSBAND","WIFE","MARRIAGE","MARR_END","CHILDREN","ERRORS"]
     for i in range(c_ind1):
         individuals[i].update_table()
     for i in range(c_fam1):
         families[i].update_table()
 
     #PRINT DATA
-    print ("INDIVIDUALS")
-    print(x)
-    print ("\nFAMILIES")
-    print(y)
+    # print ("INDIVIDUALS")
+    # print(x)
+    # print ("\nFAMILIES")
+    # print(y)
 
     #Print to output file
+
     with open("output.txt", 'w') as write_to:
         print ("INDIVIDUALS", file=write_to)
         print(x, file=write_to)
         print ("\nFAMILIES", file=write_to)
         print(y, file=write_to)
+        print("ALL ERRORS:",file=write_to)
+        for line in sorted(errors):
+            print(line,file=write_to)
 
 except FileNotFoundError:
     # File not found
